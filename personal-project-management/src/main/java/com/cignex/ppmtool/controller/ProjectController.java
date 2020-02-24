1bae466cc0164e8eb98b3fff8577dcd55f8a4a0a
Harsh Pithadiya <harsh.pithadiya@CIGNEXDATAMATICS.COM>
2020-02-24 14:04:08 +0530

    minimize the invalid json object value 2

--- personal-project-management/src/main/java/com/cignex/ppmtool/controller/ProjectController.java
+++ personal-project-management/src/main/java/com/cignex/ppmtool/controller/ProjectController.java
@@ -1,11 +1,14 @@
 package com.cignex.ppmtool.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +28,7 @@ public class ProjectController {
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 		
 		if(result.hasErrors()) {
-			return new ResponseEntity<String>("Invalid Object", HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
 		}
 		Project projectOne = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
