0ee05ac026385ce3b0012132aefb4edf0e5d9074
Harsh Pithadiya <harsh.pithadiya@CIGNEXDATAMATICS.COM>
2020-02-24 11:32:51 +0530

    created project module and define properties

--- personal-project-management/src/main/java/com/cignex/ppmtool/model/Project.java
+++ personal-project-management/src/main/java/com/cignex/ppmtool/model/Project.java
@@ -1,5 +1,121 @@
 package com.cignex.ppmtool.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+@Entity
 public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;						// store unique project id
+	private String projectName;				// store project name
+	private String projectIdentifier;		// custom identifier 
+	private String description;				// project description
+	
+	private Date start_date;				// project starting date
+	private Date end_date;					// project ending date (optional)
+	private Date created_At;				// project creation date
+	private Date updated_At;				// project update date
+
+	public Project() {
+	}
+	
+	
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+
+	public String getProjectIdentifier() {
+		return projectIdentifier;
+	}
+
+
+	public void setProjectIdentifier(String projectIdentifier) {
+		this.projectIdentifier = projectIdentifier;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public Date getStart_date() {
+		return start_date;
+	}
+
+
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+
+
+	public Date getEnd_date() {
+		return end_date;
+	}
+
+
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+
+
+	public Date getCreated_At() {
+		return created_At;
+	}
+
+
+	public void setCreated_At(Date created_At) {
+		this.created_At = created_At;
+	}
+
+
+	public Date getUpdated_At() {
+		return updated_At;
+	}
+
+
+	public void setUpdated_At(Date updated_At) {
+		this.updated_At = updated_At;
+	}
+
+
+	@PrePersist
+	protected void onCreate() {				
+		this.created_At = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updated_At = new Date();
+	}
 
 }
