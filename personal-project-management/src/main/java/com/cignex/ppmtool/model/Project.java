3f3b6995acd33ce64b480cff83d7ec03f6222203
Harsh Pithadiya <harsh.pithadiya@CIGNEXDATAMATICS.COM>
2020-02-24 13:43:03 +0530

    project object validation

--- personal-project-management/src/main/java/com/cignex/ppmtool/model/Project.java
+++ personal-project-management/src/main/java/com/cignex/ppmtool/model/Project.java
@@ -15,122 +15,104 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
-
 public class Project {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;						// store unique project id
-	
+	private long id; // store unique project id
+
 	@NotBlank(message = "Project name is required")
-	private String projectName;				// store project name
-	
+	private String projectName; // store project name
+
 	@NotBlank(message = "Project Identifier is required")
 	@Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
 	@Column(updatable = false, unique = true)
-	private String projectIdentifier;		// custom identifier 
-	
+	private String projectIdentifier; // custom identifier
+
 	@NotBlank(message = "Project Description is required")
-	private String description;				// project description
-	
+	private String description; // project description
+
 	@JsonFormat(pattern = "yyyy-mm-dd")
-	private Date start_date;				// project starting date
+	private Date start_date; // project starting date
 	@JsonFormat(pattern = "yyyy-mm-dd")
-	private Date end_date;					// project ending date (optional)
+	private Date end_date; // project ending date (optional)
 	@JsonFormat(pattern = "yyyy-mm-dd")
-	private Date created_At;				// project creation date
+	private Date created_At; // project creation date
 	@JsonFormat(pattern = "yyyy-mm-dd")
-	private Date updated_At;				// project update date
+	private Date updated_At; // project update date
 
 	public Project() {
 	}
-	
-	
+
 	public long getId() {
 		return id;
 	}
-
 
 	public void setId(long id) {
 		this.id = id;
 	}
 
-
 	public String getProjectName() {
 		return projectName;
 	}
-
 
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
 	}
 
-
 	public String getProjectIdentifier() {
 		return projectIdentifier;
 	}
-
 
 	public void setProjectIdentifier(String projectIdentifier) {
 		this.projectIdentifier = projectIdentifier;
 	}
 
-
 	public String getDescription() {
 		return description;
 	}
-
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-
 	public Date getStart_date() {
 		return start_date;
 	}
-
 
 	public void setStart_date(Date start_date) {
 		this.start_date = start_date;
 	}
 
-
 	public Date getEnd_date() {
 		return end_date;
 	}
-
 
 	public void setEnd_date(Date end_date) {
 		this.end_date = end_date;
 	}
 
-
 	public Date getCreated_At() {
 		return created_At;
 	}
-
 
 	public void setCreated_At(Date created_At) {
 		this.created_At = created_At;
 	}
 
-
 	public Date getUpdated_At() {
 		return updated_At;
 	}
-
 
 	public void setUpdated_At(Date updated_At) {
 		this.updated_At = updated_At;
 	}
 
-
 	@PrePersist
-	protected void onCreate() {				
+	protected void onCreate() {
 		this.created_At = new Date();
 	}
-	
+
 	@PreUpdate
 	protected void onUpdate() {
 		this.updated_At = new Date();
